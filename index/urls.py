831bbd1bfbdda97ae120ed3aeeb95a2ee4cc0fd4
unknown <d1360_audi@hotmail.com>
2025-04-15 15:54:04 -0600

    add effects,checkvidoes downloads,install ffmpef localy

--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 app_name = "index"
 
 urlpatterns = [
@@ -24,4 +29,4 @@ urlpatterns = [
     path('upload/comic/', views.upload_comic, name='upload_comic'),
     path('comic/<int:id>/', views.watchComic, name='watchComic'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
