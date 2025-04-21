4eb8480af2e3db944d34bbb26125bd0c5ec74f80
Salchiis <d1360_audi@hotmail.com>
2025-04-21 08:08:05 -0600

    cambios fin de seaman

--- HVIDs/settings.py
+++ HVIDs/settings.py
@@ -16,6 +16,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://2f82-177-238-22-141.ngrok-free.app",
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
