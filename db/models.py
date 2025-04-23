58866af406eb1b9b0b71d14a6e5a3f31f35aed74
unknown <d1360_audi@hotmail.com>
2025-04-23 16:03:00 -0600

    fix video and saves

--- db/models.py
+++ db/models.py
@@ -114,10 +114,10 @@ class MediaFile(models.Model):
     character= models.ManyToManyField(Character, blank=True)
 
     # Ahora se guarda el archivo localmente
-    file = models.FileField(upload_to='media_files/%Y/%m/%d/', blank=True, null=True,max_length=255)
+    file = models.FileField(upload_to='media_files/%Y%m%d/', blank=True, null=True,max_length=255)
     user=models.ForeignKey(User, on_delete=models.CASCADE,blank=True, null=True)
     # Generación automática de thumbnail
-    image = models.ImageField(upload_to='media_files/thumbnails/%Y/%m/%d/', blank=True, null=True)
+    image = models.ImageField(upload_to='media_files/thumbnails/%Y%m%d/', blank=True, null=True)
     isVideo = models.BooleanField(default=True)
     
     def __str__(self):
