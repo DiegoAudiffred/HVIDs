56639b97027cf7859c866e1597f8410eb4e9b5fe
unknown <d1360_audi@hotmail.com>
2025-04-30 16:56:52 -0600

    checar form de editar usuario

--- login/forms.py
+++ login/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Usuario', max_length=150)
-    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput,required=False )
