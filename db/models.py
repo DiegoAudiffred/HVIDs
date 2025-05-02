4c77be544c3502908a7bcbb50e59b73dcb59722c
Salchiis <d1360_audi@hotmail.com>
2025-05-02 08:23:38 -0600

    fix user change info

--- db/models.py
+++ db/models.py
@@ -17,7 +17,6 @@ class UserManager(BaseUserManager):
     use_in_migrations = True
 
     def _create_user(self, username, password, **extra_fields):
-        """Create and save a User with the given username and password."""
         if not username:
             raise ValueError('The given username must be set')
         user = self.model(username=username, **extra_fields)
@@ -26,13 +25,11 @@ class UserManager(BaseUserManager):
         return user
 
     def create_user(self, username, password=None, **extra_fields):
-        """Create and save a regular User with the given username and password."""
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
         return self._create_user(username, password, **extra_fields)
 
     def create_superuser(self, username, password, **extra_fields):
-        """Create and save a SuperUser with the given username and password."""
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
