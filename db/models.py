1886f7f209346c8675661f9a5e592a4ea058e148
unknown <d1360_audi@hotmail.com>
2025-05-05 17:00:46 -0600

    no se que hice xd

--- db/models.py
+++ db/models.py
@@ -40,22 +40,50 @@ class UserManager(BaseUserManager):
 
         return self._create_user(username, password, **extra_fields)
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 class User(AbstractUser):
     """Custom user model using username and password only."""
 
     username = models.CharField("Usuario", max_length=15, unique=True, null=False, blank=False)
-    image = models.ImageField(upload_to="uploads/gallery/",blank=True, null=True)
-    banner = models.ImageField(upload_to="uploads/gallery/",blank=True, null=True)
-    can_upload=models.BooleanField(default=False)
-    can_edit=models.BooleanField(default=False)
+    image = models.ImageField(upload_to="uploads/gallery/", blank=True, null=True)
+    banner = models.ImageField(upload_to="uploads/gallery/", blank=True, null=True)
+    can_upload = models.BooleanField(default=False)
+    can_edit = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
-    email = None  
+    # Campo para seguir usuarios
+    following = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        related_name="followers",
+        blank=True,null=True
+    )
+
+    email = None
     REQUIRED_FIELDS = []
     objects = UserManager()
-    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return str(self.username)
+
+    def follow(self, user):
+        """Seguir a otro usuario."""
+        self.following.add(user)
+
+    def unfollow(self, user):
+        """Dejar de seguir a otro usuario."""
+        self.following.remove(user)
+
+    def is_following(self, user):
+        """Verifica si sigue a un usuario."""
+        return self.following.filter(id=user.id).exists()
+
+    def is_followed_by(self, user):
+        """Verifica si es seguido por un usuario."""
+        return self.followers.filter(id=user.id).exists()
+
 class Tags(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
